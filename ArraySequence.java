1e47eeea5677a9534580e13374c764ceac25224a
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-12-08 12:38:12 -0500

    added methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,3 +1,16 @@
-public class ArraySequence {
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  private int currentIndex;
+  private int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+      data = new int[other.length];
+
+      for (int i = 0; i < other.length; i++) {
+          data[i]=other[i];
+      }
+          }
+
 
 }
