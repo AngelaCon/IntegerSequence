a318f90dc7fbabdf811f0b238281a60e38c5c94d
Angela Cai <angelacai@Acais-Mac.fios-router.home>
2020-12-07 20:57:46 -0500

    created files

---
+++ Range.java
@@ -0,0 +1,28 @@
+import java.util.NoSuchElementException;
+
+
+public class Range implements IntegerSequence{
+  private int start,end,current;
+  public Range(int start,  int end){
+      this.start =start;
+      this.end = end;
+      current = start;
+    }
+  public void reset(){
+      this.current = start;
+    }
+  public int length(){
+      return (Math.abs(start - end)+1);
+    }
+  public boolean hasNext(){
+      if ((current - end) > 0)return true;
+      return false;
+    }
+  //@throws NoSuchElementException
+  public int next(){
+      if (hasNext()) throw new NoSuchElementException("there is nothing after in this sequence");
+      current ++;
+      return (current-1);
+  }
+
+}
