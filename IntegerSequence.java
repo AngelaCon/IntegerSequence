28801b079e9f8e7cec540a8795b1b30d5f4a5d04
Angela Cai <angelacai@Acais-Mac.fios-router.home>
2020-12-07 20:50:11 -0500

    added template

---
+++ IntegerSequence.java
@@ -0,0 +1,37 @@
+import java.util.NoSuchElementException;
+
+public static void main(String[] args) {
+
+
+}
+
+public interface IntegerSequence{
+  boolean hasNext();//does the sequence have more elements?
+  int next();       //return the current value in the sequence and advances to the next element.
+  int length();     //returns the total length of the sequence
+  void reset();     //start over from the start of the sequence
+}
+
+
+public class Range implements IntegerSequence{
+  private int start,end,current;
+  public Range(int start,  int end){
+      this.start =start;
+      this.end = end;
+      current = start;
+    }
+  public void reset(){
+      this.current = start;
+    }
+  public int length(){
+      return (Math.abs(min - max)+1);
+    }
+  public boolean hasNext(){
+      if ((current - end) > 0)return true;
+      return false;
+    }
+
+  //@throws NoSuchElementException
+  public int next(){    }
+
+}
